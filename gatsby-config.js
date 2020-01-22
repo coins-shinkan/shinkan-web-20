213b87ad56d8c41f1e9fd562f44b2991ea4fb027
Sosuke Suzuki <aosukeke@gmail.com>
2020-01-22 17:45:36 +0900

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteMetadata: {
+        title: '筑波大学情報学群情報科学類2020',
+        description: '筑波大学情報学群情報科学類2020',
+    },
+    plugins: [
+        `gatsby-plugin-styled-components`,
+        `gatsby-plugin-react-helmet`,
+    ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+import Helmet from 'react-helmet';
+
+const Heading1 = styled.h1`
+    color: red;
+`;
+export default () => (
+    <>
+        <Helmet>
+            <title>つくばだいがく</title>
+        </Helmet>
+        <Heading1>つくばだいがくじょうほうがくぐんじょうほうかがくるいしんかん2020</Heading1>
+    </>
+)
