6cd90e67760d28102644e4d823217734dc38202b
Sosuke Suzuki <aosukeke@gmail.com>
2020-01-22 17:55:11 +0900

    Config eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
+/* eslint-disable */
 module.exports = {
-    siteMetadata: {
-        title: '筑波大学情報学群情報科学類2020',
-        description: '筑波大学情報学群情報科学類2020',
-    },
-    plugins: [
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-react-helmet`,
-    ],
-}
+  siteMetadata: {
+    title: "筑波大学情報学群情報科学類2020",
+    description: "筑波大学情報学群情報科学類2020"
+  },
+  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import styled from 'styled-components';
-import Helmet from 'react-helmet';
+import styled from "styled-components";
+import Helmet from "react-helmet";
 
 const Heading1 = styled.h1`
-    color: red;
+  color: red;
 `;
 export default () => (
-    <>
-        <Helmet>
-            <title>つくばだいがく</title>
-        </Helmet>
-        <Heading1>つくばだいがくじょうほうがくぐんじょうほうかがくるいしんかん2020</Heading1>
-    </>
-)
+  <>
+    <Helmet>
+      <title>つくばだいがく</title>
+    </Helmet>
+    <Heading1>
+      つくばだいがくじょうほうがくぐんじょうほうかがくるいしんかん2020
+    </Heading1>
+  </>
+);
