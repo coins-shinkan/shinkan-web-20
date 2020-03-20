56b97386a50366f374bbdf4ae9835c4693d872ec
sh1mc <s1911340@s.tsukuba.ac.jp>
2020-03-20 21:50:16 +0900

    merged villtaste/addContents

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Layout from "../components/Layout/Layout";
 import styled from "styled-components";
 import Color from "../const/Color";
@@ -63,42 +62,3 @@ export default () => (
     </Page>
   </Layout>
 );
-=======
-import styled from "styled-components";
-import Helmet from "react-helmet";
-import CoinsLayout from "../layouts/CoinsLayout";
-import faq from "../contents/faq.json";
-
-const Content = styled.div`
-  max-width: 1000px;
-  margin: auto;
-`;
-
-const Fq = ({ question, answer }) => {
-  return (
-    <>
-      <h2>{question}</h2>
-      <p>{answer}</p>
-    </>
-  );
-};
-
-export default () => {
-  return (
-    <>
-      <Helmet>
-        <title>よくある質問 - COINS新歓2020</title>
-      </Helmet>
-      <CoinsLayout>
-        <Content>
-          <h1>Frequently Asked Questions</h1>
-          <p>ふぁっきゅふぁっきゅー！ふぁ゛っ゛っ゛ぎゅー！！</p>
-          {faq.map(qaPair => (
-            <Fq question={qaPair.question} answer={qaPair.answer} />
-          ))}
-        </Content>
-      </CoinsLayout>
-    </>
-  );
-};
->>>>>>> origin/villtaste/addContents
