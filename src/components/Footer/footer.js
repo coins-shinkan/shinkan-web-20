2beaa097c299d954400ff4e26def22f0d8581f39
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2020-04-02 17:53:50 +0900

    add links

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -31,7 +31,9 @@ export default () => (
     <Wrapper>
       <OrgName>筑波大学令和2年度情報科学類新入生歓迎委員会</OrgName>
       <Text>筑波大学 情報学群 情報科学類</Text>
-      <Text>茨城県つくば市天王台1-1-1</Text>
+    <Text>茨城県つくば市天王台1-1-1</Text>
+    <Text>Twitter: <a href="https://twitter.com/2020coins">@2020coins</a></Text>
+    <Text>LINE: <a href="https://lin.ee/lWPRX87">@892epdec</a>(登録してください！)</Text>
     </Wrapper>
   </FooterRoot>
 );
